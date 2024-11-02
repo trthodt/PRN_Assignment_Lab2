7f55c05f77ca96e41c8c1f5e3a5e39635c91dbc6
ThoNT16 <truongthonguyen107@gmail.com>
2024-11-02 16:34:05 +0700

    feat: add orderdetails page

--- TECH_STORE/Tech_Store_WebApp/Pages/OrderPage/Index.cshtml.cs
+++ TECH_STORE/Tech_Store_WebApp/Pages/OrderPage/Index.cshtml.cs
@@ -4,13 +4,13 @@ using Tech_BussinessObjects;
 using Tech_Services.Implement;
 using Tech_Services.Interface;
 
-namespace Tech_Store_WebApp.Pages
+namespace Tech_Store_WebApp.Pages.OrderPage
 {
     public class OrderModel : PageModel
     {
         private readonly IOrderService _orderService;
 
-        public OrderModel (IOrderService orderService)
+        public OrderModel(IOrderService orderService)
         {
             _orderService = orderService;
         }
