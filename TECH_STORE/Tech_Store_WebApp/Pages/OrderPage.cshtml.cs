2e08076a1282855d1727e4e8fb1283fd1db90fe2
ThoNT16 <truongthonguyen107@gmail.com>
2024-11-02 15:43:29 +0700

    feat: add order page

---
+++ TECH_STORE/Tech_Store_WebApp/Pages/OrderPage.cshtml.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Tech_BussinessObjects;
+using Tech_Services.Implement;
+using Tech_Services.Interface;
+
+namespace Tech_Store_WebApp.Pages
+{
+    public class OrderModel : PageModel
+    {
+        private readonly IOrderService _orderService;
+
+        public OrderModel (IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+        public List<Order> Orders { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public int PageIndex { get; set; } = 1;
+
+
+        public async Task OnGetAsync()
+        {
+            Orders = _orderService.GetOrdersPagination(PageIndex, 5);
+        }
+    }
+}
